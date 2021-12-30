681b7f7dbac2e747cba121133d6fd46f8d521f53
Job Vink <jmcvink@gmail.com>
2021-12-30 15:18:01 +0100

    updated toast content

--- src/resources/views/tools/toast.blade.php
+++ src/resources/views/tools/toast.blade.php
@@ -18,17 +18,22 @@
                 <div class="p-4">
                     <div class="flex items-start">
                         <div class="flex-shrink-0">
-                            <!-- Heroicon name: outline/check-circle -->
-                            <svg class="h-6 w-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
+                            @if($message->type == \jobvink\tools\Models\FlashMessage::ERROR)
+                                <svg class="h-6 w-6 text-red-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            @else
+                                <svg class="h-6 w-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            @endif
                         </div>
                         <div class="ml-3 w-0 flex-1 pt-0.5">
                             <p class="text-sm font-medium text-gray-900">
-                                Successfully saved!
+                                {{ $message->type }}
                             </p>
                             <p class="mt-1 text-sm text-gray-500">
-                                Anyone with a link can now view this file.
+                                {{ $message->description }}
                             </p>
                         </div>
                         <div class="ml-4 flex-shrink-0 flex">
