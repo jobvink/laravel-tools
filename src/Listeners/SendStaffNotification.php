73c28dd6d9382fa4496593b703587d2276aa707d
Job Vink <jmcvink@gmail.com>
2022-05-19 10:53:15 +0200

    changed email settings

--- src/Listeners/SendStaffNotification.php
+++ src/Listeners/SendStaffNotification.php
@@ -28,7 +28,7 @@ class SendStaffNotification
     {
         $participant = $event->participant;
 
-        Mail::to([env('LUMC_MAIL'), env('GPRI_MAIL')])
+        Mail::to(env('LUMC_MAIL'))
             ->send(new Registartion($participant));
     }
 }
